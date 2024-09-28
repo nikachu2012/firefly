59a5e889068e53a1d555cd5b0ae2f83e1a6f7091
nikachu2012 <webmaster@nikachu.net>
2024-09-29 01:29:00 +0900

    [add] 記事の取得から解析、キャッシュを行う関数を追加

--- src/post/getPost.ts
+++ src/post/getPost.ts
@@ -1,8 +1,30 @@
 import { readFileSync } from "fs";
-import { join } from "path";
+import { join } from "path/posix";
 import { baseConfig } from "../config";
+import { parsePost } from "./parsePost";
+import { parseJSX } from "./parseJSX";
+
+export let cache: { [key: string]: PostObj } = {}
+
+export default async function getPost(filePath: string): Promise<PostObj> {
+    const targetFilePath = join(baseConfig.postsFolder, filePath, "index.md");
+
+    if (cache[filePath] !== undefined) {
+        // cached
+        console.log(`Note: ${filePath} is using cache.`)
+
+        return cache[filePath]
+    }
 
 
-export default function getPost(filePath: string): string {
-    return readFileSync(join(baseConfig.postsFolder, filePath, "index.md"), { encoding: "utf-8" });
+    // not cached
+    console.log(`Note: ${filePath} is using cache.`)
+    const markdown = readFileSync(targetFilePath, { encoding: "utf-8" }); // get markdown
+    const { html, frontMatter } = await parsePost(markdown); // generate html
+    const postJSX = parseJSX(html, filePath); // parse JSX
+
+    const newObj: PostObj = { html: html, jsx: postJSX, frontMatter: frontMatter }
+    cache[filePath] = newObj;
+
+    return newObj
 }
