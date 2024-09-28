562ada8b0dc8ce075e19d720b0b53342927a3b7c
nikachu2012 <webmaster@nikachu.net>
2024-09-29 01:32:52 +0900

    [add] 記事の解析を別関数に分離

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,7 +1,8 @@
-import { parseArticleMarkdown } from "@/src/article";
 import { baseConfig } from "@/src/config";
 import { readdirSync, readFileSync, statSync, writeFileSync } from "fs";
 import { join } from "path";
+import { parseJSX } from "@/src/post/parseJSX";
+import getPost from "@/src/post/getPost";
 import { uuidToPath } from "@/src/post/uuidToPath";
 import { Metadata } from "next";
 
@@ -9,7 +10,6 @@ export const dynamicParams = false;
 export const dynamic = 'force-static';
 
 const EXPORT_PATH = join(baseConfig.tempFolder, "uuidToPath.json");
-
 
 export async function generateStaticParams() {
     const dirList = readdirSync(baseConfig.postsFolder).filter((file) => {
@@ -37,28 +37,22 @@ export const generateMetadata = async ({ params }: { params: { slug: string } })
 }
 
 export default async function Post({ params }: { params: { slug: string } }) {
+    let postHTML: MarkdownObj;
+    let postJSX;
+
     if (baseConfig.useUuid) {
         const uuidToPath: { [key: string]: string } = JSON.parse(readFileSync(EXPORT_PATH, { encoding: "utf-8" }));
-        const markdown = getMarkdownArticle(uuidToPath[params.slug]) // get markdown
-        const articleHTML = await parseArticleMarkdown(markdown); // generate html
-        const articleJSX = parseJSX(articleHTML.html, uuidToPath[params.slug]); // parse html
-
-        return <>
-            {articleHTML.frontmatter.title}<br />
-            {articleJSX}
-            {JSON.stringify(articleHTML.frontmatter, null, 2)}<br />
-        </>;
+        postHTML = await getPost(uuidToPath[params.slug]) // get markdown
+        postJSX = parseJSX(postHTML.html, uuidToPath[params.slug]); // parse html
     }
     else {
-        const markdown = getMarkdownArticle(params.slug)
-        const articleHTML = await parseArticleMarkdown(markdown);
-        const articleJSX = parseJSX(articleHTML.html, params.slug);
-
-        return <>
-            {articleHTML.frontmatter.title}<br />
-            {articleJSX}
-            {JSON.stringify(articleHTML.frontmatter, null, 2)}<br />
-        </>;
+        postHTML = await getPost(params.slug)
+        postJSX = parseJSX(postHTML.html, params.slug);
     }
+
+    return <>
+        {postHTML.frontMatter.title}<br />
+        {postJSX}
+    </>;
 }
 
