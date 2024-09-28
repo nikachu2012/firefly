33beed0c06363ceec099190a1a94d1232fb8ee8a
nikachu2012 <webmaster@nikachu.net>
2024-09-29 01:30:37 +0900

    [add] UUIDとPathテーブルを変数上で管理するように変更

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -2,7 +2,7 @@ import { parseArticleMarkdown } from "@/src/article";
 import { baseConfig } from "@/src/config";
 import { readdirSync, readFileSync, statSync, writeFileSync } from "fs";
 import { join } from "path";
-import { parseJSX } from "@/src/JSXconverter";
+import { uuidToPath } from "@/src/post/uuidToPath";
 import getMarkdownArticle from "@/src/getMarkdownArticle";
 
 export const dynamicParams = false;
@@ -15,27 +15,8 @@ export async function generateStaticParams() {
     const dirList = readdirSync(baseConfig.postsFolder).filter((file) => {
         return statSync(join(baseConfig.postsFolder, file)).isDirectory();
     })
-
     if (baseConfig.useUuid) {
-        let uuidToPath: { [key: string]: string } = {};
-        let result: Array<string> = [];
-
-        for (const postName of dirList) {
-            const markdown = readFileSync(join(baseConfig.postsFolder, postName, "index.md"), { encoding: "utf-8" });
-            const articleHTML = await parseArticleMarkdown(markdown)
-            if (uuidToPath[articleHTML.frontmatter.uuid] === undefined) {
-                uuidToPath[articleHTML.frontmatter.uuid] = postName;
-            }
-            else {
-                throw new Error(`Duplicate UUID. ("${uuidToPath[articleHTML.frontmatter.uuid]}" and "${postName}")`)
-            }
-
-            writeFileSync(EXPORT_PATH, JSON.stringify(uuidToPath, null, 4));
-
-            result.push(articleHTML.frontmatter.uuid);
-        }
-
-        return result.map((postUuid: string) => ({
+        return Object.keys(uuidToPath).map((postUuid: string) => ({
             slug: postUuid
         }))
     }
---
+++ src/post/uuidToPath.ts
@@ -0,0 +1,35 @@
+import { readdirSync, readFileSync, statSync } from "fs";
+import { join } from "path/posix";
+import { baseConfig } from "../config";
+import { parsePost } from "./parsePost";
+
+export let uuidToPath: { [key: string]: string } = {};
+
+(async () => {
+    if (baseConfig.useUuid) {
+        const dirList = readdirSync(baseConfig.postsFolder).filter((file) => {
+            return statSync(join(baseConfig.postsFolder, file)).isDirectory();
+        })
+
+        // generate uuidToPath table and uuid list
+        for (const postName of dirList) {
+            const markdown = readFileSync(join(baseConfig.postsFolder, postName, "index.md"), { encoding: "utf-8" });
+            const post = await parsePost(markdown)
+
+            // undefined or "" uuid detect
+            if (post.frontMatter.uuid === undefined || post.frontMatter.uuid == "") {
+                throw new Error(`UUID is not defined. on ${postName}`)
+            }
+
+            // duplicate uuid detect
+            if (uuidToPath[post.frontMatter.uuid] === undefined) {
+                uuidToPath[post.frontMatter.uuid] = postName;
+            }
+            else {
+                if (uuidToPath[post.frontMatter.uuid] !== postName) {
+                    throw new Error(`Duplicate UUID. ("${uuidToPath[post.frontMatter.uuid]}" and "${postName}")`);
+                }
+            }
+        }
+    }
+})();
