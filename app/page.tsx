07f330f4d2d519f1c7cea02a4d4a784704be7c45
nikachu2012 <webmaster@nikachu.net>
2024-09-30 22:26:29 +0900

    [add] 記事一覧の表示を追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
-import { config } from "@/src/config";
+import { baseConfig, config } from "@/src/config";
+import getAllPost from "@/src/post/getAllPost";
+import Link from "next/link";
+import { join } from "path/posix";
 
-export default function Home() {
-  return (
-    <>
-      <h1>{config.title}</h1>
-
-    </>
-  );
+export default async function Home() {
+    return (
+        <>
+            <h1>{config.title}</h1>
+            <ol className="list-disc list-inside ml-5">
+                {(await getAllPost()).map((e) => {
+                    return <>
+                        <li><Link href={join(baseConfig.postsFolder, baseConfig.useUuid && e.frontMatter.uuid !== undefined ? e.frontMatter.uuid : e.filePath)}>{e.frontMatter.title}</Link></li>
+                    </>
+                })}
+            </ol>
+        </>
+    );
 }
