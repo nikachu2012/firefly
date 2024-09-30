d736844ed529bfc44a0ddd92103dc786b59885f2
nikachu2012 <webmaster@nikachu.net>
2024-09-30 21:23:53 +0900

    [add] Github markdown形式を追加

--- theme/post.tsx
+++ theme/post.tsx
@@ -1,13 +1,17 @@
-export default function PostTheme({ jsx, html, frontMatter }: PostObj) {
+import "./github-markdown.css"
 
+import "remark-github-alerts/styles/github-colors-light.css";
+import "remark-github-alerts/styles/github-base.css";
+
+
+export default function PostTheme({ jsx, html, frontMatter }: PostObj) {
     return <>
         <main className="w-4/6 m-auto">
             <div className="my-4">
                 <h1 className="text-4xl font-bold mb-2">{frontMatter.title}</h1>
                 {new Date(frontMatter.timestamp).toString()} / {frontMatter.author}
             </div>
-            <div className="">{jsx}</div>
-
+            <div className="markdown-body">{jsx}</div>
         </main>
     </>
 }
