7a193926f43867a1983f7d701496170fc10d4763
nikachu2012 <webmaster@nikachu.net>
2024-10-01 12:30:45 +0900

    [fix] 最上位要素にkeyをつけるように変更

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,17 @@ import { baseConfig, config } from "@/src/config";
 import getAllPost from "@/src/post/getAllPost";
 import Link from "next/link";
 import { join } from "path/posix";
+import React from "react";
 
 export default async function Home() {
     return (
         <>
             <h1>{config.title}</h1>
             <ul className="list-disc list-inside ml-5">
-                {(await getAllPost()).map((e, i) => {
-                    return <>
-                        <li key={i}><Link href={join(baseConfig.postsFolder, baseConfig.useUuid && e.frontMatter.uuid !== undefined ? e.frontMatter.uuid : e.filePath)}>{e.frontMatter.title}</Link></li>
-                    </>
+                {(await getAllPost()).map((e) => {
+                    return <React.Fragment key={e.filePath}>
+                        <li><Link href={join(baseConfig.postsFolder, baseConfig.useUuid && e.frontMatter.uuid !== undefined ? e.frontMatter.uuid : e.filePath)}>{e.frontMatter.title}</Link></li>
+                    </React.Fragment>
                 })}
             </ul>
         </>
